f2e2a1c551abb6d6a10e32d03ac1fa0200f8d254
shota214 <shota214@gmail.com>
2019-12-23 11:50:37 +0900

    top page header add

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow: '<i class="fas fa-chevron-left prev-arrow"></i>',
+    nextArrow: '<i class="fas fa-chevron-right next-arrow"></i>',
+    dots: true,
+  });
+});
