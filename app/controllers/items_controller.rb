ca89dbcfe35fefea9091ce35e49a04a505cd953f
shoji621 <takahiroshoji.ncu@gmail.com>
2020-01-09 16:55:20 +0900

    categoy_idのバリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,14 +16,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    # ブランドはstrでparamsにのってくるので、該当するbrand_idを探す
-    @category_id = Category.find_by(name: params[:item][:category_id]).id
-    @item = Item.new(item_params.merge(category_id: @category_id))
-    if @item.save
-      redirect_to root_path
-    else
-      flash.now[:alert] = @item.errors.full_messages
-      redirect_to action: 'new'
+    unless params[:item][:category_id] == "---"
+    # ブランドはstrでparamsにのってくるので、該当する孫category_idを探す
+      @category_id = Category.find_by(name: params[:item][:category_id]).id
+      @item = Item.new(item_params.merge(category_id: @category_id))
+      if @item.save
+        redirect_to root_path
+      else
+        flash.now[:alert] = @item.errors.full_messages
+        render :new
+      end
     end
   end
 
