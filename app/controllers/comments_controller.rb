17f649465afb9a87ad873c1884608e8b0194b4fa
shoji621 <takahiroshoji.ncu@gmail.com>
2020-01-12 21:55:49 +0900

    comment#create エラーハンドリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    respond_to do |format|
-      format.html { redirect_to item_path(params[:item_id])  }
-      format.json
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to item_path(params[:item_id])  }
+        format.json
+      end
+    else
+      flash.now[:alert] = 'コメントを入力してください。'
+      redirect_to item_path(params[:item_id])
     end
   end
 
