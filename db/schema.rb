51e23860108d2602ec2dc4595d7e80845cd8cad5
seishikasai <bluelegend.fire5@gmail.com>
2020-01-13 15:32:45 +0900

    [WIP]出品者の購入ボタンの削除

--- db/schema.rb
+++ db/schema.rb
@@ -138,6 +138,8 @@ ActiveRecord::Schema.define(version: 2020_01_12_005745) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
